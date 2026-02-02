086ceade6de5f2a23a640b428dfd917f09189417
Jeanpierre <jenisingo2@gmail.com>
2026-02-02 09:57:08 -0500

    empleado

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.forms import UsercreationFrom
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
     return render (request, "home.html")
