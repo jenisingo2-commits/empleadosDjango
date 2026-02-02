ee54702c0b2e2d7812429c3b7dbb7a9d553d1e79
Jeanpierre <jenisingo2@gmail.com>
2026-02-02 10:36:20 -0500

    empleado

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
-    return render (request, "home.html")
+    return render(request, "home.html")
 
 def signup(request):
-    return render (request, "signup.html", {"form": UserCreationForm})
+    if request.method == "GET":
+     return render (request, 
+                   "signup.html", 
+                   {"form": UserCreationForm})
+    else:
+     if request.POST["password1"] == request.POST["password2"]:
+        try: 
+            user = User.objects.create_user(
+                username=request.POST["username"],
+                password=request.POST["password1"]
+            )
+            user.save()
+            return HttpResponse("Usuario creado exitosamente")
+        
+        except:
+            return HttpResponse("Error al crear el usuario")
