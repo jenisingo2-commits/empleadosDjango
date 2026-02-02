3373e508b61e1015e126de43926eb698994f71d2
Jeanpierre <jenisingo2@gmail.com>
2026-02-02 09:48:36 -0500

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.forms import UsercreationFrom
 # Create your views here.
+def home(request):
+    return render (request, "home.html")
+
+def signup(request):
+    return render (request, "signup.html", {"form": UserCreationForm})
